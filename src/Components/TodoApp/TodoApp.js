19eb0e946efdc9771df3a770d3446b589caa5c46
anascsk <anascsk@gmail.com>
2021-08-12 22:37:33 +0530

    js update

--- src/Components/TodoApp/TodoApp.js
+++ src/Components/TodoApp/TodoApp.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
 import "./TodoApp.css";
 export default class   extends Component {
+
+    state = {
+    input: ""
+    };
+
+
+
+    handleChange = (event) => {
+        this.setState({
+            input: event.target.value
+        })
+    };
     render() {
+        const { input } = this.state;
         return (
             <div className="todo-container">
            
 
             <form className="input-section">
             <h1> Todo App</h1>
-                <input type="text" placeholder="Enter Here"  />
+                <input type="text"
+                value= {input}
+                onChange={this.handleChange} placeholder="Enter Here"  />
                 
               </form>
 
