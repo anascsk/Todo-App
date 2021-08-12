9195ff2b663c41e0cc0a224a738659c62e7ed4b1
anascsk <anascsk@gmail.com>
2021-08-12 21:54:00 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{ Component }  from 'react';
+import TodoApp from './Components/TodoApp/TodoApp';
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <TodoApp /> 
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/Components/TodoApp/TodoApp.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import "./TodoApp.css";
+export default class   extends Component {
+    render() {
+        return (
+            <div className="todo-container">
+           
+
+            <form className="input-section">
+            <h1> Todo App</h1>
+                <input type="text" placeholder="Enter Here"  />
+                
+              </form>
+
+              <ul>
+                  <li>sdjfsjkidhfsikouf</li>
+                  <li>sdjfsjkidhfsikouf</li>
+              </ul>
+            </div>
+        )
+    }
+}
